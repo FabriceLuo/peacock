067d6abf65de33d9b0efd8bf7dae6109d1a78bf4
luominghao <luo_minghao@163.com>
2017-08-01 13:24:18 +0800

    update tests tree print option
    group print tests operation by output des

--- tests_discover.py
+++ tests_discover.py
@@ -8,10 +8,38 @@ def __load_tests_from_dir(search_dir):
     
     return test_suit
 
-def __parse_tests(test_suit):
+def __parse_tests(tests_suit):
+    pass
+def __print_tests_tree(tests_tree, mode, option):
     pass
 
-def __discover_tests():
+def __get_print_option(args):
+    option = dict()
+    if 'file' not in args:
+        mode = 'stdout'
+    else:
+        mode = 'file'
+        option['path'] = args['file']
+        
+    return (mode, option)
+
+def __print_tests_to_stdout(tests_tree, option):
+    pass
+
+def __print_tests_to_file(tests_tree, option):
+    pass
+
+def __print_tests(fd, tests_tree, option):
+    pass
+
+def __discover_tests(args):
+    tests_suit = __load_tests_from_dir(args['root'])
+    
+    tests_tree = __parse_tests(tests_suit)
+    
+    #get print mode and options
+    mode, option = __get_print_option(args)
+    __print_tests_tree(tests_tree, mode, option)
     pass
 
 def main():
